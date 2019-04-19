a1ccf4d5aadb108b2b615f6a4590300cbfca7ae7
Marda <marda.firmansyah@gmail.com>
2019-04-20 00:23:46 +0700

    Initial Project

---
+++ providers/ApolloServerProvider.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/**
+ * apollo-server-adonis
+ *
+ * (c) Marda Firmansyah <marda.firmansyah@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const { ServiceProvider } = require('@adonisjs/fold')
+
+class ApolloServerProvider extends ServiceProvider {
+    /**
+     * Register AdonisApollo to the IoC container
+     * with `Adonis/Addons/ApolloServer` namespace.
+     *
+     * @method register
+     *
+     * @return {void}
+     */
+    register () {
+        this.app.singleton('Adonis/Addons/ApolloServer', () => {
+            return new (require('../src/ApolloServer'))()
+        })
+
+        this.app.alias('Adonis/Addons/ApolloServer', 'ApolloServer')
+    }
+}
+
+module.exports = ApolloServerProvider
